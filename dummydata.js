3f67ce7a434c24d0d7fe703cc22c6780113c192d
devbxtzz <batztonnie@gmail.com>
2022-11-03 10:40:28 +0300

    routes

---
+++ dummydata.js
@@ -0,0 +1,34 @@
+const dummyData = [
+  {
+    title: "flight to jordan",
+    time: "10am",
+    price: 21000,
+    date: "26-11-2022",
+  },
+  {
+    title: "flight to dubai",
+    time: "12pm",
+    price: 33000,
+    date: "26-11-2022",
+  },
+  {
+    title: "flight to nairobi",
+    time: "11pm",
+    price: 9000,
+    date: "27-11-2022",
+  },
+  {
+    title: "flight to kigali",
+    time: "1am",
+    price: 8000,
+    date: "27-11-2022",
+  },
+  {
+    title: "flight to new york",
+    time: "3pm",
+    price: 52000,
+    date: "27-11-2022",
+  },
+];
+
+module.exports = dummyData;
---
+++ models/Flight.js
@@ -0,0 +1,10 @@
+exports.flights = [
+  {
+    id: "flight-1656022957355",
+    title: "flight to dubai",
+    time: "12pm",
+    price: 33000,
+    date: "26-11-2022",
+  },
+];
+
